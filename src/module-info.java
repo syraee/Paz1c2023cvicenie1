3f3ead0453e37353a095cffef7180ef6e99c97a4
student <student@158.197.34.226>
2023-09-19 12:47:48 +0200

    initial commit

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Paz1c2023cvicenie {
+}
---
+++ src/paz1c/First.java
@@ -0,0 +1,10 @@
+package paz1c;
+
+
+public class First {
+	
+	public static void main(String[] args){
+		System.out.println("Hello PAZ1c");
+	}
+
+}
